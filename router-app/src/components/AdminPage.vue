50fffc530b795b9460c3ca538ebadf6005fe5241
nz <nazmulhasanhimel@yahoo.com>
2019-06-24 21:12:14 +0600

    show all users in admin page

--- router-app/src/components/AdminPage.vue
+++ router-app/src/components/AdminPage.vue
@@ -2,17 +2,51 @@
   <div class="hello">
     <h1>Welcome to administrator page</h1>
     <h2>{{msg}}</h2>
+  
+    <ul v-for="user in users" :key="user._id">
+    <li>{{ user.firstname }}
+    </li>
+</ul>
+  
   </div>
+  
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
     data () {
       return {
-        msg: 'The Admin'
+        users: {},
+        msg: 'The Admin',
+        
       }
+    },
+  
+  
+
+  created () {
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+
+  methods: {
+    fetchData () {
+      let me = this;
+      axios.get('/api/users/detailall').then((resp) => {
+          me.users = resp.data.users;
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     }
   }
+}
 </script>
 <style scoped>
   h1, h2 {
