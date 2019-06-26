d2d9ffb92fb1faf2a862e130dcab2f5ea1ba49b8
nz <nazmulhasanhimel@yahoo.com>
2019-06-26 21:36:40 +0600

    added delete and used socket in admin

--- router-app/src/components/AdminPage.vue
+++ router-app/src/components/AdminPage.vue
@@ -5,6 +5,7 @@
   
     <ul v-for="user in users" :key="user._id">
     <li>{{ user.firstname }}
+      <button @click="Delete($event, user._id)">Delete</button>
     </li>
 </ul>
   
@@ -14,13 +15,15 @@
 
 <script>
 import axios from 'axios'
+import _ from 'underscore'
+import io from 'socket.io-client'
 
   export default {
     data () {
       return {
         users: {},
         msg: 'The Admin',
-        
+        socket: io('localhost:3000')
       }
     },
   
@@ -28,6 +31,10 @@ import axios from 'axios'
 
   created () {
     this.fetchData()
+    this.socket.emit('message', 'hello');
+    this.socket.on('mess',function(msg){
+      console.log(msg);
+    });
   },
 
   watch: {
@@ -44,9 +51,29 @@ import axios from 'axios'
         .catch((err) => {
           console.log(err)
         })
-    }
+    },
+    
+    Delete(e,id) {
+        // axios.delete('api/'+user_id+'/delete').then(resp) => { me.users = resp.data.users;})
+        // .catch((err) => {
+        //   console.log(err)
+        // })
+        //e.target.parentElement.remove();
+        let me= this;
+        axios.delete('api/users/'+id+'/delete').then((resp) => {
+          // me.users = resp.data.users;
+          me.users= _.reject(me.users,function(user){
+            return user._id===id;
+
+          })
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
   }
-}
+  }
 </script>
 <style scoped>
   h1, h2 {
