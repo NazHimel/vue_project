6e913b79ba0635c56895169204dde6fc94ea3416
nz <nazmulhasanhimel@yahoo.com>
2019-07-01 21:49:51 +0600

    added chat events

--- backend/services/socketManager.js
+++ backend/services/socketManager.js
@@ -10,10 +10,17 @@ module.exports = function(server){
             console.log('disconnected socket');
         });
 
+        
+        socket.on('joinChannel', function(data){
+            console.log('New user Joined:' ,JSON.stringify(data));
+            //app.connections[socket.id]= data;
+            socket.join(data.roomName);
+        });
+
         socket.on('message', function(msg){
             console.log(msg);
+            io.to(msg.room).emit('message', msg);
         });
-        socket.emit('mess', 'hi');
     });
 
 
