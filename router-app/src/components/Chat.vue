9030d9435936f46433bf38250be2bfdf2175a1e3
nz <nazmulhasanhimel@yahoo.com>
2019-07-01 21:52:12 +0600

    created group, and send msg in group

--- router-app/src/components/Chat.vue
+++ router-app/src/components/Chat.vue
@@ -5,8 +5,10 @@
         <option value="common">Common</option>
         <option value="public">Public</option>
     </select>
-    <button class="btn btn-primary" v-on:click="join">Join
-    </button>
+    <button class="btn btn-primary" v-on:click="join">Join</button>
+    <div> {{msgFromOther}} </div>
+    <input type="text" v-model="text"/>
+    <button class="btn btn-primary" v-on:click="send">Send</button>
   </div>
   
 </template>
@@ -22,18 +24,34 @@ import io from 'socket.io-client'
     
       return {
         room:'',
-        socket: io('localhost:3000')
+        socket: io('localhost:3000'),
+        text:'',
+        msgFromOther:'',
+        user: JSON.parse(localStorage.getItem('user'))
       }
     },
     created(){
+        
         this.socket.on('connect', function(){
             console.log('connected');
+        });
+        this.socket.on('message', data=>{
+            if(data.from!= this.user._id){
+
+            this.msgFromOther = this.msgFromOther+ '<br/>'+ data.text;
+            
+            }
+            console.log(data);
         });
 
     },
   methods: {
       join: function (e){
           this.socket.emit('joinChannel', {roomName: this.room});
+      },
+      send: function(e){
+          
+          this.socket.emit('message', {text: this.text, from: this.user._id, room: this.room});
       }
     
   }
